19bc3ade7cb84567a1493d1d8dbef50aabc88a9c
marvelous micheal <donatusmarvelous09@gmail.com>
2023-10-06 08:57:03 -0700

    this isry very tight this

--- src/assets/components/menu-container/MenuContainer.jsx
+++ src/assets/components/menu-container/MenuContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Menubtn from './menuButton/menubtn'
-import MenuContent from './menuContent/menuContent'
+import Menubtn from './menuButton/Menubtn'
+import MenuContent from './menuContent/MenuContent'
 
 function MenuContainer() {
   return (
